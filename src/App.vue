e651bc11b0c0d874dc6c5ea90cb2dc8b501d3d05
kai <kaienyeo225@gmail.com>
2023-05-23 21:41:02 +0800

    auto icon import

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
 </script>
 
 <template>
-  <div>
+  <div class="app">
     Hello
+    <div>
+      <i-simple-line-icons-logout></i-simple-line-icons-logout>
+      <i-simple-line-icons-arrow-left-circle></i-simple-line-icons-arrow-left-circle>
+      <i-simple-line-icons-anchor></i-simple-line-icons-anchor>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.app {
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+  min-width: 100vw;
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        IconsResolver(),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
 })
